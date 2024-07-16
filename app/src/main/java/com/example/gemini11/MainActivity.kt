7bd693ba79598e5cfb8aaaaaf3b0287103cf4ccb
KOUSHAMBHA15IT <KOUSHAMBHA39@GMAIL.COM>
2024-07-16 21:32:54 +0530

    gemini

---
+++ app/src/main/java/com/example/gemini11/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.gemini11
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.ai.client.generativeai.BuildConfig
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        model()
+    }
+
+
+    fun model(){
+        var textV= findViewById<TextView>(R.id.texty);
+        val generativeModel = GenerativeModel(
+            // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+            modelName = "gemini-1.5-flash",
+            // Access your API key as a Build Configuration variable (see "Set up your API key" above)
+            apiKey = "AIzaSyCEjD0yoiBUDBnGCWTy-606Fw-5nnF39eY"
+        )
+        val prompt = "Write a story about a magic backpack."
+        MainScope().launch {
+            val response = generativeModel.generateContent(prompt)
+            textV.setText(response.text);
+
+        }
+
+
+
+
+
+    }
+}
